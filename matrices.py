5b834079070806e7e71da9e150fab0ffe5d52630
nicelaserguy <jens.gumm@physik.tu-darmstadt.de>
2025-03-18 09:30:09 +0100

    implemented PSO algorithm for optimization of resonator parameters

--- matrices.py
+++ matrices.py
@@ -30,3 +30,74 @@ class Matrices:
         """Calculate the q-parameter of a resonator"""
         m = self.resonator_matrix(r1, r2, d, n1, n2)
         return (m[0, 1] * n2 - m[1, 0]) / (m[0, 0] - m[1, 1])
+    
+    def roundtrip_tangential(self, nc, lc, n0, l1, l3, r1_tan, r2_tan, theta):
+        """Calculate the roundtrip matrix for the tangential plane"""
+        l2 = (2 * l1 + lc + l3) / (2 * np.cos(theta))
+        return np.dot(
+            self.free_space(lc / 2, nc),
+            np.dot(
+                self.free_space(l1, n0),
+                np.dot(
+                    self.curved_mirror_tangential(r1_tan, theta),
+                    np.dot(
+                        self.free_space(l2, n0),
+                        np.dot(
+                            self.curved_mirror_tangential(r2_tan, theta),
+                            np.dot(
+                                self.free_space(l3, n0),
+                                np.dot(
+                                    self.curved_mirror_tangential(r2_tan, theta),
+                                    np.dot(
+                                        self.free_space(l2, n0),
+                                        np.dot(
+                                            self.curved_mirror_tangential(r1_tan, theta),
+                                            np.dot(
+                                                self.free_space(l1, n0),
+                                                self.free_space(lc / 2, nc),
+                                            ),
+                                        ),
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        )
+
+
+    def roundtrip_sagittal(self, nc, lc, n0, l1, l3, r1_sag, r2_sag, theta):
+        """Calculate the roundtrip matrix for the sagittal plane"""
+        l2 = (2 * l1 + lc + l3) / (2 * np.cos(theta))
+        return np.dot(
+            self.free_space(lc / 2, nc),
+            np.dot(
+                self.free_space(l1, n0),
+                np.dot(
+                    self.curved_mirror_sagittal(r1_sag, theta),
+                    np.dot(
+                        self.free_space(l2, n0),
+                        np.dot(
+                            self.curved_mirror_sagittal(r2_sag, theta),
+                            np.dot(
+                                self.free_space(l3, n0),
+                                np.dot(
+                                    self.curved_mirror_sagittal(r2_sag, theta),
+                                    np.dot(
+                                        self.free_space(l2, n0),
+                                        np.dot(
+                                            self.curved_mirror_sagittal(r1_sag, theta),
+                                            np.dot(
+                                                self.free_space(l1, n0),
+                                                self.free_space(lc / 2, nc),
+                                            ),
+                                        ),
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        )
--- resonators.py
+++ resonators.py
@@ -1,7 +1,7 @@
 import numpy as np
-import deap
-
 from deap import base, creator, tools, algorithms
+from deap.benchmarks.tools import diversity, convergence
+from deap.benchmarks import sphere
 from matrices import Matrices
 
 class Resonator:
@@ -13,109 +13,135 @@ class Resonator:
     def set_ui_resonator(self, ui_resonator):
         """Set the ui_resonator reference"""
         self.ui_resonator = ui_resonator
-    
-    def roundtrip_tangential(self, nc, lc, n0, l1, l2, l3, r1_tan, r2_tan, r3_tan, r4_tan, theta1):
-        """Calculate the roundtrip matrix for the tangential plane"""
-        return np.dot(
-            self.matrices.free_space(nc, lc/2),
-            np.dot(
-                self.matrices.free_space(n0, l1),
-                np.dot(
-                    self.matrices.curved_mirror_tangential(r4_tan, theta1/2),
-                    np.dot(
-                        self.matrices.free_space(n0, l2),
-                        np.dot(
-                            self.matrices.curved_mirror_tangential(r3_tan, theta1/2),
-                            np.dot(
-                                self.matrices.free_space(n0, l3),
-                                np.dot(
-                                    self.matrices.curved_mirror_tangential(r2_tan, theta1/2),
-                                    np.dot(
-                                        self.matrices.free_space(n0, l2),
-                                        np.dot(
-                                            self.matrices.curved_mirror_tangential(r1_tan, theta1/2),
-                                            np.dot(
-                                                self.matrices.free_space(n0, l1),
-                                                self.matrices.free_space(nc, lc/2)))))))))))
-        
-    def roundtrip_sagittal(self, nc, lc, n0, l1, l2, l3, r1_sag, r2_sag, r3_sag, r4_sag, theta1):
-        """Calculate the roundtrip matrix for the sagittal plane"""
-        return np.dot(
-            self.matrices.free_space(nc, lc/2),
-            np.dot(
-                self.matrices.free_space(n0, l1),
-                np.dot(
-                    self.matrices.curved_mirror_sagittal(r4_sag, theta1/2),
-                    np.dot(
-                        self.matrices.free_space(n0, l2),
-                        np.dot(
-                            self.matrices.curved_mirror_sagittal(r3_sag, theta1/2),
-                            np.dot(
-                                self.matrices.free_space(n0, l3),
-                                np.dot(
-                                    self.matrices.curved_mirror_sagittal(r2_sag, theta1/2),
-                                    np.dot(
-                                        self.matrices.free_space(n0, l2),
-                                        np.dot(
-                                            self.matrices.curved_mirror_sagittal(r1_sag, theta1/2),
-                                            np.dot(
-                                                self.matrices.free_space(n0, l1),
-                                                self.matrices.free_space(nc, lc/2)))))))))))
 
         
-    def waist_sagittal(self, z):
-        """Calculate the sagittal waist of a resonator in the center of the crystal"""
-        # TODO: Implement the waist calculation
-        return 0
-        
-    
     def get_input(self):
+        """Inputs from UI converted to meters"""
         self.target_sag = float(self.ui_resonator.edit_target_waist_sag.text())*1e-6
         self.target_tan = float(self.ui_resonator.edit_target_waist_tan.text())*1e-6
-        self.nco = float(self.ui_resonator.edit_crystal_refractive_index_ordinary.text())
-        self.nce = float(self.ui_resonator.edit_crystal_refractive_index_extraordinary.text())
+        self.nc = float(self.ui_resonator.edit_crystal_refractive_index_ordinary.text())
         self.lc = float(self.ui_resonator.edit_crystal_length.text())*1e-3
         self.wavelength = float(self.ui_resonator.edit_wavelength.text())*1e-6
         self.n_prop = 1
-        
-        return np.array([self.target_sag, self.target_tan, self.nco, self.nce, self.lc, self.n_prop, self.wavelength])
-        
+
+        return np.array([self.target_sag, self.target_tan, self.nc, self.lc, self.n_prop, self.wavelength])
+
     def evaluate_resonator(self):
         inputs = self.get_input()
-        target_sag, target_tan, nco, nce, lc, n_prop, wavelength = inputs
+        target_sag, target_tan, nc, lc, n_prop, wavelength = inputs
 
-        # Define the DEAP algorithm
+        # Define the fitness function
+        def objective(individual):
+            l1, l3, theta, r1_sag, r1_tan, r2_sag, r2_tan = individual
+            roundtrip_matrix_sag = self.matrices.roundtrip_sagittal(nc, lc, n_prop, l1, l3, r1_sag, r2_sag, theta)
+            roundtrip_matrix_tan = self.matrices.roundtrip_tangential(nc, lc, n_prop, l1, l3, r1_tan, r2_tan, theta)
+            m_sag = np.abs(roundtrip_matrix_sag[0, 0] - roundtrip_matrix_sag[1, 1])
+            m_tan = np.abs(roundtrip_matrix_tan[0, 0] - roundtrip_matrix_tan[1, 1])
+            b_sag = np.abs(roundtrip_matrix_sag[0, 1])
+            b_tan = np.abs(roundtrip_matrix_tan[0, 1])
+            waist_sag = np.sqrt((b_sag * wavelength) / (np.pi) * (np.sqrt(np.abs(1 / (1 - m_sag**2)))))
+            waist_tan = np.sqrt((b_tan * wavelength) / (np.pi) * (np.sqrt(np.abs(1 / (1 - m_tan**2)))))
+            '''weigthing the waist sizes (smaller waist is more important)'''
+            if waist_sag < waist_tan:
+                fitness_value = np.sqrt(
+                    2*((waist_sag - target_sag) / target_sag)**2 +
+                    ((waist_tan - target_tan) / target_tan)**2 +
+                    (1 / 100) * m_sag**2 +
+                    (1 / 100) * m_tan**2
+                )
+                return fitness_value,
+            if waist_sag > waist_tan:
+                fitness_value = np.sqrt(
+                    ((waist_sag - target_sag) / target_sag)**2 +
+                    2*((waist_tan - target_tan) / target_tan)**2 +
+                    (1 / 100) * m_sag**2 +
+                    (1 / 100) * m_tan**2
+                )
+                return fitness_value,
+
+            else:
+                fitness_value = np.sqrt(
+                    ((waist_sag - target_sag) / target_sag)**2 +
+                    ((waist_tan - target_tan) / target_tan)**2 +
+                    (1 / 100) * m_sag**2 +
+                    (1 / 100) * m_tan**2
+                )
+                return fitness_value,
+
+        # DEAP setup for PSO
         creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-        creator.create("Individual", list, fitness=creator.FitnessMin)
+        creator.create("Particle", list, fitness=creator.FitnessMin, speed=list, smin=None, smax=None, best=None)
+
+        def generate(size, pmin, pmax, smin, smax):
+            particle = creator.Particle(np.random.uniform(pmin, pmax) for _ in range(size))
+            particle.speed = [np.random.uniform(smin, smax) for _ in range(size)]
+            particle.smin = smin
+            particle.smax = smax
+            return particle
+
+        def update_particle(part, best, phi1, phi2):
+            u1 = np.random.uniform(0, phi1, len(part))
+            u2 = np.random.uniform(0, phi2, len(part))
+            v_u1 = [u * (b - p) for u, b, p in zip(u1, part.best, part)]
+            v_u2 = [u * (b - p) for u, b, p in zip(u2, best, part)]
+            part.speed = [v + vu1 + vu2 for v, vu1, vu2 in zip(part.speed, v_u1, v_u2)]
+
+            for i, speed in enumerate(part.speed):
+                if speed < part.smin:
+                    part.speed[i] = part.smin
+                elif speed > part.smax:
+                    part.speed[i] = part.smax
+
+            part[:] = [p + v for p, v in zip(part, part.speed)]
 
         toolbox = base.Toolbox()
-        toolbox.register("attr_float", np.random.uniform, 0.1, 10.0)
-        toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, n=4)
-        toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+        toolbox.register("particle", generate, size=7, pmin=0.025, pmax=1.0, smin=-0.5, smax=0.5)
+        toolbox.register("population", tools.initRepeat, list, toolbox.particle)
+        toolbox.register("update", update_particle, phi1=1.5, phi2=1.5)
+        toolbox.register("evaluate", objective)
 
-        def evaluate(individual):
-            l1, l2, l3, theta1 = individual
-            # TODO: Implenent the radius of curvature calculation from .json file
-            roundtrip_matrix_sag = self.roundtrip_sagittal(nco, lc, n_prop, l1, l2, l3, 1, 1, 1, 1, theta1)
-            roundtrip_matrix_tan = self.roundtrip_tangential(nco, lc, n_prop, l1, l2, l3, 1, 1, 1, 1, theta1)
-            waist_sag = np.abs(roundtrip_matrix_sag[0, 0] - roundtrip_matrix_sag[1, 1])
-            waist_tan = np.abs(roundtrip_matrix_tan[0, 0] - roundtrip_matrix_tan[1, 1])
-            return (np.abs(waist_sag - target_sag) + np.abs(waist_tan - target_tan),)
+        # Create the population
+        population = toolbox.population(n=200)
 
-        toolbox.register("mate", tools.cxBlend, alpha=0.5)
-        toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.2)
-        toolbox.register("select", tools.selTournament, tournsize=3)
-        toolbox.register("evaluate", evaluate)
+        # PSO loop
+        best = None
+        for gen in range(1000):
+            for part in population:
+                part.fitness.values = toolbox.evaluate(part)
+                # Aktualisiere das beste Partikel basierend auf Minimierung
+                if not part.best or part.best.fitness.values[0] > part.fitness.values[0]:
+                    part.best = creator.Particle(part)
+                    part.best.fitness.values = part.fitness.values
+                if not best or best.fitness.values[0] > part.fitness.values[0]:
+                    best = creator.Particle(part)
+                    best.fitness.values = part.fitness.values
 
-        population = toolbox.population(n=100)
-        ngen = 100
-        cxpb = 0.5
-        mutpb = 0.2
+            for part in population:
+                toolbox.update(part, best)
 
-        algorithms.eaSimple(population, toolbox, cxpb, mutpb, ngen, verbose=True)
+            # Debugging-Ausgabe
+            print(f"Generation {gen}: Best fitness = {best.fitness.values[0]}")
 
-        best_individual = tools.selBest(population, 1)[0]
-        print("l1: %s\nl2: %s\nl3: %s\ntheta: %s\nwith fitness: %s" % (best_individual[0], best_individual[1], best_individual[2], best_individual[3], best_individual.fitness.values))
-
-
-        return best_individual
+        # Output results
+        l1, l3, theta, r1_sag, r1_tan, r2_sag, r2_tan = best
+        '''Calculate the waist sizes'''
+        roundtrip_matrix_sag = self.matrices.roundtrip_sagittal(nc, lc, n_prop, l1, l3, r1_sag, r2_sag, theta)
+        roundtrip_matrix_tan = self.matrices.roundtrip_tangential(nc, lc, n_prop, l1, l3, r1_tan, r2_tan, theta)
+        m_sag = np.abs(roundtrip_matrix_sag[0, 0] - roundtrip_matrix_sag[1, 1])
+        m_tan = np.abs(roundtrip_matrix_tan[0, 0] - roundtrip_matrix_tan[1, 1])
+        b_sag = np.abs(roundtrip_matrix_sag[0, 1])
+        b_tan = np.abs(roundtrip_matrix_tan[0, 1])
+        waist_sag = np.sqrt((b_sag * wavelength) / (np.pi) * (np.sqrt(np.abs(1 / (1 - m_sag**2)))))
+        waist_tan = np.sqrt((b_tan * wavelength) / (np.pi) * (np.sqrt(np.abs(1 / (1 - m_tan**2)))))
+        print(f"Best solution found:")
+        print(f"l1: {l1*1e3} mm")
+        print(f"l3: {l3*1e3} mm")
+        print(f"theta: {np.rad2deg(theta)} deg")
+        print(f"r1_sag: {r1_sag*1e3} mm")
+        print(f"r1_tan: {r1_tan*1e3} mm")
+        print(f"r2_sag: {r2_sag*1e3} mm")
+        print(f"r2_tan: {r2_tan*1e3} mm")
+        print(f"waist_sag: {waist_sag*1e6} um")
+        print(f"waist_tan: {waist_tan*1e6} um")
+        print(f"Fitness: {best.fitness.values[0]}")
+        return best
